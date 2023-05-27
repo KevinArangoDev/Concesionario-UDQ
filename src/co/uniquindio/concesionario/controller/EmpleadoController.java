4e7b13eba563b921fb202e70024e6777809f386e
Kevin Velasquez <Keviin.stiiven99@gmail.com>
2023-05-27 17:38:40 -0500

    Actualización Ventana Empleado

--- src/co/uniquindio/concesionario/controller/EmpleadoController.java
+++ src/co/uniquindio/concesionario/controller/EmpleadoController.java
@@ -1,12 +1,18 @@
 package co.uniquindio.concesionario.controller;
 
 import com.jfoenix.controls.JFXButton;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class EmpleadoController implements Initializable {
 
@@ -36,18 +42,75 @@ public class EmpleadoController implements Initializable {
 
     @FXML
     void ventVenderVehiculo(ActionEvent event) {
+    	try {
 
+    		FXMLLoader loader = new FXMLLoader(
+    				getClass().getResource("../view/VenderVehiculoView.fxml"));
+    		Parent root = loader.load();
+
+    		LoginAdministradorController controlador = loader.getController();
+
+    		Scene scene = new Scene(root);
+    		Stage stage = new Stage();
+
+    		stage.setScene(scene);
+    		stage.show();
+    		stage.setTitle("Car UQ");
+    		Stage myStage = (Stage) this.btnVenderVehiculo.getScene().getWindow();
+    		myStage.close();
+
+    	} catch (IOException e) {
+            e.printStackTrace();
+    	}
     }
 
     @FXML
     void ventAlquilarVehiculo(ActionEvent event) {
+    	try {
 
+    		FXMLLoader loader = new FXMLLoader(
+    				getClass().getResource("../view/AlquilarVehiculoView.fxml"));
+    		Parent root = loader.load();
+
+    		LoginAdministradorController controlador = loader.getController();
+
+    		Scene scene = new Scene(root);
+    		Stage stage = new Stage();
+
+    		stage.setScene(scene);
+    		stage.show();
+    		stage.setTitle("Car UQ");
+    		Stage myStage = (Stage) this.btnAlquilarVehiculo.getScene().getWindow();
+    		myStage.close();
+
+    	} catch (IOException e) {
+            e.printStackTrace();
+    	}
     }
 
 
     @FXML
     void ventComprarVehiculo(ActionEvent event) {
+    	try {
 
+    		FXMLLoader loader = new FXMLLoader(
+    				getClass().getResource("../view/ComprarVehiculoView.fxml"));
+    		Parent root = loader.load();
+
+    		LoginAdministradorController controlador = loader.getController();
+
+    		Scene scene = new Scene(root);
+    		Stage stage = new Stage();
+
+    		stage.setScene(scene);
+    		stage.show();
+    		stage.setTitle("Car UQ");
+    		Stage myStage = (Stage) this.btnComprarVehiculo.getScene().getWindow();
+    		myStage.close();
+
+    	} catch (IOException e) {
+            e.printStackTrace();
+    	}
     }
 
 
